49b337b50b60572c6f3c420d7cd934651fb276b2
Stanislav Nowogrudski <dj.steam@gmail.com>
2018-01-11 17:43:52 +0100

    Update runConsumer.java
    [review] add some todo's

--- src/main/java/de/hpi/KafkaClient/runConsumer.java
+++ src/main/java/de/hpi/KafkaClient/runConsumer.java
@@ -1,7 +1,9 @@
 package de.hpi.KafkaClient;
 
 import java.util.function.Consumer;
-
+// TODO - java classes must have a capital letter as a fist
+// TODO - use slf4j for logging, do not use System.out.println
+// TODO - format the braces
 public class runConsumer
 {
 
