117770ce5f5d1485ed46f814b6323b5c1cae50a5
Stanislav Nowogrudski <dj.steam@gmail.com>
2018-01-11 17:45:25 +0100

    Update runProducer.java
    [review] add some todo's

--- src/main/java/de/hpi/KafkaClient/runProducer.java
+++ src/main/java/de/hpi/KafkaClient/runProducer.java
@@ -1,10 +1,11 @@
 package de.hpi.KafkaClient;
 
 import org.apache.log4j.BasicConfigurator;
-
+// TODO - java classes must have a capital letter as a fist
+// TODO - format the braces
 public class runProducer
 {   public static void main(String[] args) {
-        //BasicConfigurator.configure();
+        //BasicConfigurator.configure(); // do not comment out the code, delete it
         ConfigurableProducer producer = new ConfigurableProducer("15");
         producer.createTopic("test1234", 4 , 1);
         for (int i =0; i<100; i++){
