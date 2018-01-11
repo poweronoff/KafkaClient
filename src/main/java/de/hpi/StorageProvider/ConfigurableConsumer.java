6a3b969d1661bf43135b39e55383c6fd55fc8089
Tom.Schwarzburg <tom.schwarzburg@stuudent.hpi.de>
2018-01-11 10:27:50 +0100

    Finally working

--- src/main/java/de/hpi/StorageProvider/ConfigurableConsumer.java
+++ src/main/java/de/hpi/StorageProvider/ConfigurableConsumer.java
@@ -8,25 +8,25 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.log4j.BasicConfigurator;
 
 public class ConfigurableConsumer {
     public static void main(String[] args) throws Exception {
 
-        String topicName = "test";
+        String topicName = "test123";
         Properties props = new Properties();
 
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "ts1552.byod.hpi.de:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put("group.id", "test");
+
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
 
         consumer.subscribe(Arrays.asList(topicName));
-
-        System.out.println("Subscribed to topic " + topicName);
         while (true) {
            ConsumerRecords<String, String> records = consumer.poll(100);
             for (ConsumerRecord<String, String> record : records)
--- src/main/java/de/hpi/StorageProvider/ConfigurableProducer.java
+++ src/main/java/de/hpi/StorageProvider/ConfigurableProducer.java
@@ -1,7 +1,9 @@
 package de.hpi.StorageProvider;
 
 import java.util.Properties;
+import java.util.concurrent.Future;
 
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 import kafka.admin.AdminUtils;
 import kafka.admin.RackAwareMode;
 import kafka.utils.ZKStringSerializer$;
@@ -12,19 +14,26 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.KafkaException;
+import org.apache.kafka.common.KafkaFuture;
+import org.apache.kafka.common.errors.AuthorizationException;
+import org.apache.kafka.common.errors.OutOfOrderSequenceException;
+import org.apache.kafka.common.errors.ProducerFencedException;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.log4j.BasicConfigurator;
 
 public class ConfigurableProducer {
+
 
 
     public void sendToKafka(String topic, String message) {
 
         /*
         TO DO: get properties from a config file
-         */
+
         Properties props = new Properties();
 
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "ts1552.byod.hpi.de:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.18.20.117:9092");
         props.put(ProducerConfig.ACKS_CONFIG, "all");
         props.put(ProducerConfig.RETRIES_CONFIG, 0);
         props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
@@ -35,11 +44,28 @@ public class ConfigurableProducer {
         Producer<String, String> producer = new KafkaProducer<String, String>(props);
 
         producer.send(new ProducerRecord<String, String>(topic, message));
-        System.out.println("Message sent successfully");
+        producer.flush();
+        producer.close(); */
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "ts1552.byod.hpi.de:9092");
+        props.put("transactional.id", "0");
+        props.put("group.id", "test");
 
+        Producer<String, String> producer = new KafkaProducer<>(props, new StringSerializer(), new StringSerializer());
 
-        producer.close();
-    }
+        producer.initTransactions();
+
+        try {
+            producer.beginTransaction();
+            for (int i = 0; i < 100; i++)
+                producer.send(new ProducerRecord<>(topic, Integer.toString(i), Integer.toString(i)));
+            producer.commitTransaction();
+        } catch (ProducerFencedException | OutOfOrderSequenceException | AuthorizationException e) {
+            producer.close();
+        } catch (KafkaException e) {
+            producer.abortTransaction();
+        }
+        producer.close();    }
 
     public void createTopic(String topic, int partitions, int replication) {
         ZkClient zkClient = null;
